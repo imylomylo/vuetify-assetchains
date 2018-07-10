8549f2c52a5863f060479c7d7f5a37f3e0c9d33c
imylomylo <imylomylo@gmail.com>
2018-07-11 01:20:30 +1000

    Add toolbar

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,43 @@
 <template>
   <v-app>
+    <v-toolbar>
+      <v-toolbar-side-icon></v-toolbar-side-icon>
+      <v-toolbar-title>Title</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items class="hidden-sm-and-down">
+        <v-btn flat>Link One</v-btn>
+        <v-btn flat>Link Two</v-btn>
+        <v-btn flat>Link Three</v-btn>
+      </v-toolbar-items>
+    </v-toolbar>
     <HelloWorld></HelloWorld>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+import HelloWorld from "./components/HelloWorld";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
     HelloWorld
   },
-  data () {
+  data() {
     return {
       clipped: false,
       drawer: true,
       fixed: false,
-      items: [{
-        icon: 'bubble_chart',
-        title: 'Assetchain Wizard'
-      }],
+      items: [
+        {
+          icon: "bubble_chart",
+          title: "Assetchain Wizard"
+        }
+      ],
       miniVariant: false,
       right: true,
       rightDrawer: false,
-      title: 'Assetchain Wizard'
-    }
+      title: "Assetchain Wizard"
+    };
   }
-}
+};
 </script>
